33a7f401055c95ad6d146dad5b512cd6b18af498
JeffBenson <68038932+JeffBenson@users.noreply.github.com>
2020-08-10 12:23:15 -0700

    Add files via upload

---
+++ Remove a Specified Character/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_as_Specified_Character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method for removing a character from a string
+            string remove_char(string str, int n)
+            {
+                return str.Remove(n, 1);
+            }
+
+            Console.WriteLine("Input a word to remove the first letter.");
+            Console.WriteLine("\n___________________________________________________\n");
+            Console.WriteLine("Please enter a word: ");
+            string inputOne = Console.ReadLine();
+
+            Console.WriteLine("\nRemoving 1st letter: \n");
+            Console.WriteLine(remove_char(inputOne, 0));
+
+            Console.ReadLine();
+        }
+    }
+}
