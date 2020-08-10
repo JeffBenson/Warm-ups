cf20ffe7321623a3e9ab2de481a0ab8619e3a750
JeffBenson <68038932+JeffBenson@users.noreply.github.com>
2020-08-10 12:25:46 -0700

    Add files via upload

---
+++ LetterSwap - Switches the First and Last Letter of a user's/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterSwap___Switches_the_First_and_Last_Letter_of_a_user_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switch the First and Last letter's of a user's input.");
+            Console.WriteLine("\n________________________________________________________\n");
+            //Method for swapping letters
+            string firstLast(string ustr)
+            {
+                return ustr.Length > 1
+                    ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1) : ustr;
+            }
+
+            Console.WriteLine("Please enter a word: ");
+
+            string input = Console.ReadLine();
+            Console.WriteLine(firstLast(input));
+            Console.ReadKey();
+        }
+    }
+}
