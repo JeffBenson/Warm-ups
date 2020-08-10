c20412d9c7e7c800819ac3f7ead605d0662091f2
JeffBenson <68038932+JeffBenson@users.noreply.github.com>
2020-08-10 12:25:05 -0700

    Add files via upload

---
+++ Swapping Numbers in Memory/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NumberSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Warmup - Number Swap
+            try
+            {
+                int number1, number2, temp;
+
+                Console.WriteLine("Swapping Numbers in Memory");
+                Console.WriteLine("________________________________________________");
+                Console.WriteLine("\nInput the First Number: ");
+                number1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nInput the Second Number: ");
+                number2 = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+                Console.WriteLine("Before Swap:\nNumber1 = {0}\nNumber2 = {1}.", number1, number2);
+
+
+                //Swap Values
+                temp = number1;
+                number1 = number2;
+                number2 = temp;
+
+                //After Swap
+                Console.WriteLine("\n\nAfter swap:\nNumber1 = {0}\nNumber2 = {1}.", number1, number2);
+
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.Clear();
+                Console.WriteLine("An error has occurred. Exiting now.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
